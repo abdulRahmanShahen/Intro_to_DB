b59010f95b3d4ca0f62e2004d2e18079552a3d8c
Abdul-Raman Shaheen <abdalrahmanshaheen9@gmail.com>
2025-07-27 21:58:32 +0200

    me

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,15 +1,11 @@
-# Define global conversion factors
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-FAHRENHEIT_FREEZING_POINT = 32
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
-# Convert Fahrenheit to Celsius
 def convert_to_celsius(fahrenheit):
-    return (fahrenheit - FAHRENHEIT_FREEZING_POINT) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
-# Convert Celsius to Fahrenheit
 def convert_to_fahrenheit(celsius):
-    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + FAHRENHEIT_FREEZING_POINT
+    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
 def main():
     try:
@@ -20,10 +16,10 @@ def main():
 
         if unit == 'F':
             result = convert_to_celsius(temperature)
-            print(f"{temperature}°F is {result:.2f}°C")
+            print(f"{temperature}°F is {result}°C")
         elif unit == 'C':
             result = convert_to_fahrenheit(temperature)
-            print(f"{temperature}°C is {result:.2f}°F")
+            print(f"{temperature}°C is {result}°F")
         else:
             print("Invalid unit. Please enter 'C' for Celsius or 'F' for Fahrenheit.")
     except ValueError:
@@ -31,4 +27,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
